12e17c9bfa7eee70fdb2a8856e9ffa0c1184bf41
Joel Wong <joelwongjy@gmail.com>
2021-06-09 02:52:18 +0800

    Install tailwindcss and craco
    Install tailwind css for styling and craco to integrate tailwind to ionic.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,8 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
+/* TailwindCSS */
+import './theme/tailwind.css';
 /* Theme variables */
 import './theme/variables.css';
 
