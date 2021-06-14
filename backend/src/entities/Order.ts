4371ecfdd928187995c89bb3891fcae882d6a09e
Joel Wong <joelwongjy@gmail.com>
2021-06-15 05:32:26 +0800

    feat: add get methods for order entity

--- backend/src/entities/Order.ts
+++ backend/src/entities/Order.ts
@@ -1,8 +1,9 @@
 import { IsNotEmpty } from "class-validator";
-import { Column, Entity, ManyToOne, OneToMany } from "typeorm";
+import { Column, Entity, getRepository, ManyToOne, OneToMany } from "typeorm";
 import { Discardable } from "./Discardable";
-import { Jio } from "./Jio"
+import { Jio } from "./Jio";
 import { Item } from "./Item";
+import { OrderData, OrderListData } from "src/types/orders";
 
 @Entity()
 export class Order extends Discardable {
@@ -27,4 +28,33 @@ export class Order extends Discardable {
 
   @ManyToOne((type) => Jio, (jio) => jio.orders)
   jio!: Jio;
+
+  getItems = async (): Promise<Item[]> => {
+    const items = (
+      await getRepository(Order).findOneOrFail({
+        where: { id: this.id },
+        relations: ["items"],
+      })
+    ).items;
+    return items;
+  };
+
+  getListData = async (): Promise<OrderListData> => {
+    const items = this.items || (await this.getItems());
+    const cost = items.map((item) => item.cost ?? 0).reduce((a, b) => a + b);
+    return {
+      ...this.getBase(),
+      paid: this.paid,
+      itemCount: items.length,
+      cost,
+    };
+  };
+
+  getData = async (): Promise<OrderData> => {
+    const items = this.items || (await this.getItems());
+    return {
+      ...(await this.getListData()),
+      items,
+    };
+  };
 }
