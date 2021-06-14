a2b46ed35fb4554193f0438068bac3a9d46fdaa9
Joel Wong <joelwongjy@gmail.com>
2021-06-15 05:34:16 +0800

    feat: add api routes for jios and orders

--- backend/src/routes/api/jios.ts
+++ backend/src/routes/api/jios.ts
@@ -5,10 +5,11 @@ import { BearerTokenType } from "../../types/tokens";
 
 export const router = Router();
 
-//router.use(checkBearerToken(BearerTokenType.AccessToken));
-router.get("/", JioController.index);
+router.use(checkBearerToken(BearerTokenType.AccessToken));
 router.get("/:id", JioController.show);
+router.get("/user/:id", JioController.showUserJios);
 router.post("/create", JioController.create);
+router.patch("/:id", JioController.edit);
 router.delete("/:id", JioController.remove);
 
 export default router;
