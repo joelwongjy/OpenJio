818c7fb7dc7f97b5f70f6ffc78d0c24fc5850859
Joel Wong <joelwongjy@gmail.com>
2021-06-21 02:01:06 +0800

    feat: add alert utils
    Add function to get alert callback

--- frontend/src/utils/alertUtils.ts
+++ frontend/src/utils/alertUtils.ts
@@ -1,1 +1,49 @@
+import { RouteState } from 'interfaces/routes/common';
+
+export function getAlertCallback(
+  setState: <T extends RouteState>(state: Partial<T>) => void,
+): (
+  isAlertOpen: boolean,
+  hasConfirm: boolean,
+  alertHeader: string,
+  alertMessage: string,
+  confirmHandler?: (() => void) | undefined,
+  cancelHandler?: (() => void) | undefined
+) => void {
+  const alertCallback = (
+    isAlertOpen: boolean,
+    hasConfirm: boolean,
+    alertHeader: string,
+    alertMessage: string,
+    confirmHandler?: () => void,
+    cancelHandler?: () => void,
+  ) => {
+    setState({
+      isAlertOpen,
+      hasConfirm,
+      alertHeader,
+      alertMessage,
+    });
+    if (confirmHandler) {
       setState({
+        confirmHandler: () => {
+          confirmHandler();
+          setState({ isAlertOpen: false });
+        },
+      });
+    } else {
+      setState({ confirmHandler: () => setState({ isAlertOpen: false }) });
+    }
+    if (cancelHandler) {
+      setState({
+        cancelHandler: () => {
+          cancelHandler();
+          setState({ isAlertOpen: false });
+        },
+      });
+    } else {
+      setState({ cancelHandler: () => setState({ isAlertOpen: false }) });
+    }
+  };
+  return alertCallback;
+}
