cff2823d82d38a3b21951016bdd05e07f3f0b020
Josephaedan <48595194+Josephaedan@users.noreply.github.com>
2021-06-20 16:27:22 +0800

    Revamped Authentification Page
    Changelog:
    - Reinstalled Tailwind.config.js to full customisability
    - Added "orange" colour to Tailwind
    
    - Changed Authentication pages to background image + Box
    - Changed Button colours to orange
    - Changed Text colour to orange

--- frontend/src/components/loading/Loading.tsx
+++ frontend/src/components/loading/Loading.tsx
@@ -10,7 +10,7 @@ const Loading: React.FunctionComponent = () => {
         crossOrigin="anonymous"
       />
       <div className="w-full h-full fixed block top-0 left-0 bg-white opacity-75 z-50">
-        <span className="text-purple-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0">
+        <span className="text-orange opacity-75 align-center align-middle top-1/2 text-center my-0 mx-auto block relative w-0 h-0">
           <i className="fas fa-circle-notch fa-spin fa-5x" />
         </span>
       </div>
--- frontend/src/routes/signup/Signup.tsx
+++ frontend/src/routes/signup/Signup.tsx
@@ -36,171 +36,176 @@ const Signup: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-sm w-full space-y-8">
-        <div>
-          <img
-            className="mx-auto h-12 w-auto"
-            src="/images/hamburger.svg"
-            alt="OpenJio"
-          />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Create Your Account
-          </h2>
-        </div>
-        <form className="mt-8 space-y-6" onSubmit={handleSignup}>
-          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-1">
-            <input type="hidden" name="remember" value="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
-              <div>
-                <label htmlFor="name" className="sr-only">
-                  Name
-                </label>
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  autoComplete="name"
-                  required
-                  className="h-12 appearance-none leading-8 rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Name"
-                  value={name}
-                  onChange={(e) => {
-                    clearError();
-                    setName(e.target.value!);
-                  }}
-                />
+    <div className="min-h-screen flex items-center justify-center lg:bg-food-background bg-gray-50 bg-cover py-12 px-4 sm:px-6 lg:px-8">
+      <div className="absolute bg-black opacity-0 lg:opacity-30 inset-0 z-0" />
+      <div className="max-w-md w-full space-y-8 p-10 bg-gray-50 rounded-xl lg:shadow-lg z-10">
+        <div className="max-w-sm w-full space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="/images/hamburger.svg"
+              alt="OpenJio"
+            />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Create Your Account
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={handleSignup}>
+            <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-1">
+              <input type="hidden" name="remember" value="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label htmlFor="name" className="sr-only">
+                    Name
+                  </label>
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    autoComplete="name"
+                    required
+                    className="h-12 appearance-none leading-8 rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-orange focus:border-orange focus:z-10 sm:text-sm"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => {
+                      clearError();
+                      setName(e.target.value!);
+                    }}
+                  />
 
-                <label htmlFor="username" className="sr-only">
-                  Username
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="text"
-                  autoComplete="username"
-                  required
-                  className="h-12 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Username"
-                  value={username}
-                  onChange={(e) => {
-                    clearError();
-                    setUsername(e.target.value!);
-                  }}
-                />
+                  <label htmlFor="username" className="sr-only">
+                    Username
+                  </label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="text"
+                    autoComplete="username"
+                    required
+                    className="h-12 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-orange focus:border-orange focus:z-10 sm:text-sm"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => {
+                      clearError();
+                      setUsername(e.target.value!);
+                    }}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="email" className="sr-only">
+                    E-mail Address
+                  </label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="text"
+                    autoComplete="email-address"
+                    required
+                    className="h-12 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-orange focus:border-orange focus:z-10 sm:text-sm"
+                    placeholder="E-mail address"
+                    value={email}
+                    onChange={(e) => {
+                      clearError();
+                      setEmail(e.target.value!);
+                    }}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="h-12 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-orange focus:border-orange focus:z-10 sm:text-sm"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => {
+                      clearError();
+                      setPassword(e.target.value!);
+                    }}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="confirm-password" className="sr-only">
+                    Confirm Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    className="h-12 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-orange focus:border-orange focus:z-10 sm:text-sm"
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={(e) => {
+                      clearError();
+                      setConfirmPassword(e.target.value!);
+                    }}
+                  />
+                </div>
               </div>
 
               <div>
-                <label htmlFor="email" className="sr-only">
-                  E-mail Address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="text"
-                  autoComplete="email-address"
-                  required
-                  className="h-12 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="E-mail address"
-                  value={email}
-                  onChange={(e) => {
-                    clearError();
-                    setEmail(e.target.value!);
-                  }}
-                />
-              </div>
-
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="h-12 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => {
-                    clearError();
-                    setPassword(e.target.value!);
-                  }}
-                />
-              </div>
-
-              <div>
-                <label htmlFor="confirm-password" className="sr-only">
-                  Confirm Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  className="h-12 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Confirm Password"
-                  value={confirmPassword}
-                  onChange={(e) => {
-                    clearError();
-                    setConfirmPassword(e.target.value!);
-                  }}
-                />
+                <button
+                  type="submit"
+                  className="h-12 group relative w-full inline-flex items-center justify-center py-2 px-4 border border-transparent text-md font-medium rounded-md text-white bg-orange hover:bg-orange focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange"
+                >
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                    <svg
+                      className="h-5 w-5 text-orange group-hover:text-orange"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                  Sign Up
+                </button>
               </div>
             </div>
+          </form>
 
-            <div>
-              <button
-                type="submit"
-                className="h-12 group relative w-full inline-flex items-center justify-center py-2 px-4 border border-transparent text-md font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          <div className="flex items-center">
+            <p className="mr-1 text-sm text-gray-900">
+              Already have an account?
+            </p>
+            <div className="text-sm">
+              <a
+                href="/login"
+                className="font-medium text-orange hover:text-orange"
               >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <svg
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </span>
-                Sign Up
-              </button>
+                Sign in
+              </a>
             </div>
           </div>
-        </form>
 
-        <div className="flex items-center">
-          <p className="mr-1 text-sm text-gray-900">Already have an account?</p>
-          <div className="text-sm">
-            <a
-              href="/login"
-              className="font-medium text-indigo-600 hover:text-indigo-500"
+          {isError ? (
+            <div
+              className="text-sm font-medium text-red-500"
+              style={{
+                marginTop: '0.5rem',
+                visibility: isError ? 'visible' : 'hidden',
+              }}
             >
-              Sign in
-            </a>
-          </div>
+              {errorMessage}
+            </div>
+          ) : (
+            <div />
+          )}
         </div>
-
-        {isError ? (
-          <div
-            className="text-sm font-medium text-red-500"
-            style={{
-              marginTop: '0.5rem',
-              visibility: isError ? 'visible' : 'hidden',
-            }}
-          >
-            {errorMessage}
-          </div>
-        ) : (
-          <div />
-        )}
       </div>
     </div>
   );
